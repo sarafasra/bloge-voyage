fd51b75978b90b7e8bcedc13732dd32229ad4a71
Hamza <hamzazaidi253@gmail.com>
2026-02-05 12:26:22 +0100

    ajouter le button supprimer

--- script.js
+++ script.js
@@ -17,6 +17,16 @@ function afficherCards(tripsArray) {
         container.innerHTML += `
         
         <div class="mx-5 w-[350px] h-[150px] md:w-[250px] mt-16 mb-14">
+        <div class="relative">
+                <button onclick="leMenu(this)"
+                    class="absolute top-2 right-2 text-white text-xl bg-black/40 rounded-full px-2">
+                    <i class="fa-solid fa-ellipsis-vertical"></i>
+                </button>
+                <div class="hidden absolute top-10 right-2 bg-white shadow rounded w-32">
+                    <button class="block w-full text-left px-3 py-2 hover:bg-gray-100">Modifier</button>
+                    <button onclick="deletetrips(${trip.id})" class="block w-full text-left px-3 py-2 text-red-600 hover:bg-gray-100">Supprimer</button>
+                </div>
+            </div>
             <div class="bg-cover bg-center rounded-xl shadow-xl w-full h-full"
                  style="background-image:url('${trip.image}')"></div>
             <h6 class="font-semibold mt-1">${trip.title}</h6>
@@ -50,5 +60,12 @@ document.getElementById("trip-form").addEventListener("submit", function (e) {
     hid.classList.add("hidden");
 });
 
-
+function leMenu(button) {
+    const menu = button.nextElementSibling;
+    menu.classList.toggle("hidden");
+}
+function deletetrips(id){
+    trips = trips.filter(trip=>trip.id !== id );
+    afficherCards(trips);
+}
 
