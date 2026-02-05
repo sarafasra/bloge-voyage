cce0fd8d3063ff7c10b5cc5f3c4c24c3eefac025
Hamza <hamzazaidi253@gmail.com>
2026-02-05 11:24:34 +0100

    ajouter des modification sur html

---
+++ script.js
@@ -0,0 +1,54 @@
+let trips = [
+    {
+        id: 1,
+        title: "Yellow River",
+        author: "Joan Garcia",
+        image: "/tele.jpg",
+        category: "River"
+    }
+];
+
+let container = document.getElementById("posts-container");
+let hid = document.getElementById("hidden-form");
+
+function afficherCards(tripsArray) {
+    container.innerHTML = "";
+    tripsArray.forEach(trip => {
+        container.innerHTML += `
+        
+        <div class="mx-5 w-[350px] h-[150px] md:w-[250px] mt-16 mb-14">
+            <div class="bg-cover bg-center rounded-xl shadow-xl w-full h-full"
+                 style="background-image:url('${trip.image}')"></div>
+            <h6 class="font-semibold mt-1">${trip.title}</h6>
+            <p class="text-sm">by ${trip.author}</p>
+            
+        </div>
+        `;
+
+    });
+}
+
+afficherCards(trips);
+
+document.getElementById("btnAdd").addEventListener("click", () => {
+    hid.classList.remove("hidden");
+});
+
+document.getElementById("trip-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const newTrip = {
+        id: trips.length + 1,
+        title: e.target.title.value,
+        author: e.target.author.value,
+        image: e.target.image.value,
+        category: e.target.category.value
+    };
+
+    trips.push(newTrip);
+    afficherCards(trips);
+    e.target.reset();
+    hid.classList.add("hidden");
+});
+
+
---
+++ test.js
@@ -0,0 +1,5 @@
+let name = "Sara";
+let greeting = `Bonjour, ${name} !`;
+console.log(greeting);
+
+    
